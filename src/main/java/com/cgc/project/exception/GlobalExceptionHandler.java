ae9bbf5b55c0ed8efba1d95f7dc18d7dbeccf97b
lfd <1480144633@qq.com>
2024-04-15 23:05:09 +0800

    第二天

---
+++ src/main/java/com/cgc/project/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.cgc.project.exception;
+
+import com.cgc.project.common.BaseResponse;
+import com.cgc.project.common.ErrorCode;
+import com.cgc.project.common.ResultUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理器
+ *
+ * @author cgc
+ */
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public BaseResponse<?> businessExceptionHandler(BusinessException e) {
+        log.error("businessException: " + e.getMessage(), e);
+        return ResultUtils.error(e.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public BaseResponse<?> runtimeExceptionHandler(RuntimeException e) {
+        log.error("runtimeException", e);
+        return ResultUtils.error(ErrorCode.SYSTEM_ERROR, e.getMessage());
+    }
+}
---
+++ src/main/java/com/cgc/project/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.cgc.project.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.cgc.project.model.entity.User;
+
+/**
+ * @Entity com.cgc.project.model.domain.User
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
